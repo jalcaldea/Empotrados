81309af2c9fe16b274afd5c4aec9eb549c70df0c
Jesús Alcalde <jesus.alcalde.a@gmail.com>
2015-02-02 13:58:54 +0100

    Añadido proyecto básico de Arduino

---
+++ proyecto/proyecto.ino
@@ -0,0 +1,23 @@
+/*
+  Empotrados
+  Prctica de empotrados
+  
+  Modulo principal de la aplicacion
+  del juego de mesa con Arduino
+
+  by:
+  - Jesus Alcalde
+  - Adrian Gutierrez
+  - Javier Jimenez
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+}
